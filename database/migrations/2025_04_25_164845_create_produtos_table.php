897aad7913b3725c9700bcc7ffe2a358b1831170
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-04-25 14:08:19 -0300

    :sparkles: feat: add migrations

---
+++ database/migrations/2025_04_25_164845_create_produtos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->decimal('valor');
+            $table->string('descricao');
+            $table->string('imagens');
+            $table->foreignId('confeitaria_id')->references('id')->on('confeitaria');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
